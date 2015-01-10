bb0a118be4e6aa8755170b89f7bcc1e03ca934bf
Urec2731 <chman@ukr.net>
2015-01-10 05:47:37 +0200

    GridFs

--- server/server22.js
+++ server/server22.js
@@ -25,6 +25,11 @@ var conn = mongoose.connection;
 //});
 
 
+// create a user model
+var User = mongoose.model('User', {
+    oauthID: Number,
+    name: String
+});
 
 
 
@@ -41,15 +46,110 @@ var schemafs = new mongoose.Schema({
 });
 
 var fsfiles = mongoose.model('my_collection.files',schemafs);
-var fsfile = new fsfiles;
 
 app.set('view engine', 'jade');
+
+app.use(require('express-session')({
+    key: 'session',
+    secret: 'SUPER SECRET SECRET',
+    store: require('mongoose-session')(mongoose, {ttl: 3600, modelName : 'Session'}),
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //app.use(allowCrossDomain);
     //  app.use(multer({ dest: './tmp/'}));
 //app.use(bodyParser({ uploadDir: path.join(__dirname, 'files'), keepExtensions: true }));
 //app.use(bodyParser({ uploadDir: '~/WebstormProjects/oggfiller'.join(__dirname, 'files'), keepExtensions: true }));
 //app.use(methodOverride());
+
+// config passport
+passport.use(new FacebookStrategy({
+        clientID: config.facebook.clientID,
+        clientSecret: config.facebook.clientSecret,
+        callbackURL: config.facebook.callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOne({ oauthID: profile.id }, function(err, user) {
+            if(err) { console.log(err); }
+            if (!err && user != null) {
+                done(null, user);
+            } else {
+                var user = new User({
+                    oauthID: profile.id,
+                    name: profile.displayName,
+                    created: Date.now()
+                });
+                user.save(function(err) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("saving user ...");
+                        done(null, user);
+                    };
+                });
+            };
+        });
+    }
+));
+
+
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+    console.log('serializeUser: ' + user._id)
+    done(null, user._id);
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user){
+        console.log(user)
+        if(!err) done(null, user);
+        else done(err, null)
+    })
+});
+
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/logined',
+        failureRedirect: '/login_incorrect' }));
+
+//-----------------------------------------------------
+
+app.get('/logined', function (req, res) {
+
+    res.render('logined');
+
+});
+
+app.get('/login_incorrect', function (req, res) {
+
+    res.render('login_incorrect');
+
+});
+
+
+/*
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '' }),
+    function(req, res) {
+        res.redirect('/account');
+    });
+
+*/
+
 
 app.post('/fileupload', function(req, res, next){
 
